92531fc9a1fef0affacd91e3828adc6eb3f007a6
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2022-05-21 23:54:49 +0300

    start touching angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'my-first-app changed';
+  test = 'some test text';
+  name = 'Nikolay';
 }
