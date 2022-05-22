bdcf51420643db39bccccd8691d5b7f99ff75402
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2022-05-22 23:53:35 +0300

    preparing our projects add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app changed';
-  test = 'some test text';
-  name = 'Nikolay';
+  // title = 'my-first-app changed';
+  // test = 'some test text';
+  // name = 'Nikolay';
 }
