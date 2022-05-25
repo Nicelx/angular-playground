57fe790595c995e41ff3051f249b59bbbcd78a9e
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2022-05-25 23:52:08 +0300

    playing with selectors and small asignment

---
+++ src/app/asignment/asignment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'asignment-component',
+	templateUrl: './asignment.component.html',
+	styleUrls: ['./asignment.component.css'],
+})
+
+export class AsignmentComponent {
+
+}
