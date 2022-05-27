6ef1a37f7e5fa2620824579db3599e31f3851738
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2022-05-27 23:30:43 +0300

    data binding, string interpolation example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,20 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { AsignmentComponent } from './asignment/asignment.component';
 
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
     AsignmentComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
-  BrowserModule,
+BrowserModule,
     // FormsModule
   ],
   providers: [],
