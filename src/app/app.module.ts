e6e4391d72398f52154995c2910e4ad5a2ffafb6
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2022-06-10 23:18:44 +0300

    splitting new semi-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { AsignmentComponent } from './asignment/asignment.component';
 
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    AsignmentComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
-BrowserModule,
-    FormsModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
