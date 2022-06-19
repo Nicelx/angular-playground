57ca41b2a923963de89ad161ee0d949baf6a4686
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2022-06-19 23:55:01 +0300

    try asignment, create components and start with event emitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { GameControlComponent } from './asignment/game-control/game-control.component';
+import { OddComponent } from './asignment/odd/odd.component';
+import { EvenComponent } from './asignment/even/even.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CockpitComponent,
-    ServerElementComponent
+    ServerElementComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/asignment/game-control/game-control.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Output() gameStarted = new EventEmitter<{increment: number}>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
