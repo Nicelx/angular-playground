5ebd58423358639a34f3f47eaa27712dacb5fc0b
Nicelx <relush321@gmail.com>
2022-07-27 23:50:22 +0300

    custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import {interval, Subscription} from 'rxjs';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { interval, Observable, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: "app-home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  private firstObsSubscription: Subscription;
+	private firstObsSubscription: Subscription;
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+	ngOnInit() {
+		// this.firstObsSubscription = interval(1000).subscribe(count => {
+		//   console.log(count);
+		// })
+		const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+			setInterval(() => {
+        observer.next(count);
+        count++
+      }, 1000);
+		});
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
     })
-  }
-  ngOnDestroy(): void {
-    this.firstObsSubscription.unsubscribe();
-  }
-
+	}
+	ngOnDestroy(): void {
+		this.firstObsSubscription.unsubscribe();
+	}
 }
