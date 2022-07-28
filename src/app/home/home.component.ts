f912dd277c8dd75f6aa65e75b6e9829c53882856
Nicelx <relush321@gmail.com>
2022-07-28 21:24:32 +0300

    error, completions, operators, subjects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { interval, Observable, Subscription } from "rxjs";
+import { filter, map } from "rxjs/operators";
 
 @Component({
 	selector: "app-home",
@@ -16,15 +17,40 @@ export class HomeComponent implements OnInit, OnDestroy {
 		//   console.log(count);
 		// })
 		const customIntervalObservable = Observable.create((observer) => {
-      let count = 0;
+			let count = 0;
 			setInterval(() => {
-        observer.next(count);
-        count++
-      }, 1000);
+				observer.next(count);
+				if (count === 5) {
+					observer.complete();
+				}
+				if (count > 3) {
+					observer.error(new Error("count is greater than 3!!!!!"));
+				}
+				count++;
+			}, 1000);
 		});
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    })
+
+		this.firstObsSubscription = customIntervalObservable
+			.pipe(
+				filter((data) => {
+					return data > 0;
+				}),
+				map((data: number) => {
+					return "Round " + (data + 1);
+				})
+			)
+			.subscribe(
+				(data) => {
+					console.log(data);
+				},
+				(error) => {
+					console.log(error);
+					alert(error.message);
+				},
+				() => {
+					console.log("completed!");
+				}
+			);
 	}
 	ngOnDestroy(): void {
 		this.firstObsSubscription.unsubscribe();
