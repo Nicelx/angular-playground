041ef99dd9b26bf2d7afd4cbadb56bffe6abb730
Nicelx <relush321@gmail.com>
2022-08-02 23:31:48 +0300

    more with template-driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,16 @@ export class AppComponent implements OnInit, OnDestroy {
 	private activatedSub: Subscription;
 	@ViewChild('f') signupForm: NgForm;
 	defaultQuestion = 'pet';
+	answer = '';
+	genders = ['male', 'female'];
+	user = {
+		username: '',
+		email: '',
+		secretQuestion: '',
+		answer: '',
+		gender: '',
+	}
+	submitted = false;
 
 
 	constructor(private userService: UserService) {}
@@ -29,12 +39,32 @@ export class AppComponent implements OnInit, OnDestroy {
 
 	suggestUserName() {
 		const suggestedName = "Superuser";
+		// this.signupForm.setValue({
+		// 	userData : {
+		// 		username : suggestedName,
+		// 		email: '',
+		// 	},
+		// 	secret: 'pet',
+		// 	questionAnswer : '',
+		// 	gender: 'male',
+		// })
+		this.signupForm.form.patchValue({
+			userData : {
+				username: suggestedName
+			}
+		})
 	}
 	// onSubmit(form: NgForm) {
 	// 	console.log(form);
 	// }
 	onSubmit() {
-		console.log(this.signupForm);
+		this.submitted = true;
+		this.user.username = this.signupForm.value.userData.username;
+		this.user.email = this.signupForm.value.userData.email;
+		this.user.secretQuestion = this.signupForm.value.secret;
+		this.user.answer = this.signupForm.value.questionAnswer;
+		this.user.gender = this.signupForm.value.gender;
+		this.signupForm.reset();
 	}
 
 }
