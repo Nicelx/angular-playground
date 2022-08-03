ad877a9e53de6fd380978d0d02151100ce004e1f
Nicelx <relush321@gmail.com>
2022-08-03 23:19:24 +0300

    reactive driven approach, small setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { UserService } from "./user.service";
 import { OnDestroy } from "@angular/core";
 import { Subscription } from "rxjs";
-import { NgForm } from "@angular/forms";
+import { FormControl, FormGroup, NgForm } from "@angular/forms";
 
 @Component({
 	selector: "app-root",
@@ -10,61 +10,61 @@ import { NgForm } from "@angular/forms";
 	styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit, OnDestroy {
-	userActivated = false;
-	private activatedSub: Subscription;
-	@ViewChild('f') signupForm: NgForm;
-	defaultQuestion = 'pet';
-	answer = '';
-	genders = ['male', 'female'];
-	user = {
-		username: '',
-		email: '',
-		secretQuestion: '',
-		answer: '',
-		gender: '',
-	}
-	submitted = false;
+	// userActivated = false;
+	// private activatedSub: Subscription;
+	// @ViewChild('f') signupForm: NgForm;
+	// defaultQuestion = 'pet';
+	// answer = '';
+	// user = {
+		// 	username: '',
+		// 	email: '',
+		// 	secretQuestion: '',
+		// 	answer: '',
+		// 	gender: '',
+		// }
+		// submitted = false;
+		genders = ['male', 'female'];
+	signupForm : FormGroup;
 
 
 	constructor(private userService: UserService) {}
 
-	ngOnInit() {
-		this.activatedSub = this.userService.activatedEmitter.subscribe((didActivate) => {
-			this.userActivated = didActivate;
-		});
-	}
-	ngOnDestroy(): void {
-		this.activatedSub.unsubscribe();
-	}
+	// ngOnInit() {
+	// 	this.activatedSub = this.userService.activatedEmitter.subscribe((didActivate) => {
+	// 		this.userActivated = didActivate;
+	// 	});
+	// }
+	// ngOnDestroy(): void {
+	// 	this.activatedSub.unsubscribe();
+	// }
 
-	suggestUserName() {
-		const suggestedName = "Superuser";
-		// this.signupForm.setValue({
-		// 	userData : {
-		// 		username : suggestedName,
-		// 		email: '',
-		// 	},
-		// 	secret: 'pet',
-		// 	questionAnswer : '',
-		// 	gender: 'male',
-		// })
-		this.signupForm.form.patchValue({
-			userData : {
-				username: suggestedName
-			}
+	// suggestUserName() {
+	// 	const suggestedName = "Superuser";
+	// 	this.signupForm.form.patchValue({
+	// 		userData : {
+	// 			username: suggestedName
+	// 		}
+	// 	})
+	// }
+	// onSubmit() {
+	// 	this.submitted = true;
+	// 	this.user.username = this.signupForm.value.userData.username;
+	// 	this.user.email = this.signupForm.value.userData.email;
+	// 	this.user.secretQuestion = this.signupForm.value.secret;
+	// 	this.user.answer = this.signupForm.value.questionAnswer;
+	// 	this.user.gender = this.signupForm.value.gender;
+	// 	this.signupForm.reset();
+	// }
+
+	ngOnInit(): void {
+		this.signupForm = new FormGroup({
+			'username' : new FormControl(null),
+			'email' : new FormControl(null),
+			'gender' : new FormControl('male'), 
 		})
 	}
-	// onSubmit(form: NgForm) {
-	// 	console.log(form);
-	// }
-	onSubmit() {
-		this.submitted = true;
-		this.user.username = this.signupForm.value.userData.username;
-		this.user.email = this.signupForm.value.userData.email;
-		this.user.secretQuestion = this.signupForm.value.secret;
-		this.user.answer = this.signupForm.value.questionAnswer;
-		this.user.gender = this.signupForm.value.gender;
-		this.signupForm.reset();
+	ngOnDestroy(): void {
+		
 	}
 
 }
