2c0175fd29debb1ea05873e08c98dc312452c9ee
Nicelx <relush321@gmail.com>
2022-08-05 23:49:20 +0300

    getting access and grouping controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,8 +58,10 @@ export class AppComponent implements OnInit, OnDestroy {
 
 	ngOnInit(): void {
 		this.signupForm = new FormGroup({
-			'username' : new FormControl(null, Validators.required),
-			'email' : new FormControl(null, [Validators.required, Validators.email]),
+			'userData' : new FormGroup({
+				'username' : new FormControl(null, Validators.required),
+				'email' : new FormControl(null, [Validators.required, Validators.email]),
+			}),
 			'gender' : new FormControl('male'), 
 		})
 	}
