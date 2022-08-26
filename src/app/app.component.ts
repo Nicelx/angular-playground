85f39ecb072bafce76278cc184a321d5bb6a3661
Nicelx <relush321@gmail.com>
2022-08-26 23:56:43 +0300

    fetching data, using operators to transform responseData

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit} from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
 	selector: "app-root",
@@ -8,23 +8,53 @@ import { HttpClient } from '@angular/common/http';
 	styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-	// http
 	constructor(private http: HttpClient) {}
 
 	loadedPosts = [];
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.fetchPosts();
+	}
 
 	onCreatePost(postData: { title: string; content: string }) {
 		// Send Http request
-		console.log(postData);
+		this.http
+			.post(
+				"https://angular-test-afd6e-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
+				postData
+			)
+			.subscribe((responseData) => {
+				console.log(responseData);
+			});
 	}
 
 	onFetchPosts() {
 		// Send Http request
+		this.fetchPosts();
 	}
 
 	onClearPosts() {
 		// Send Http request
+	}
+
+	private fetchPosts() {
+		this.http
+			.get(
+				"https://angular-test-afd6e-default-rtdb.europe-west1.firebasedatabase.app/posts.json"
+			)
+			.pipe(
+				map((responseData) => {
+					const postsArray = [];
+					for (const key in responseData) {
+						if (responseData.hasOwnProperty(key)) {
+							postsArray.push({ ...responseData[key], id: key });
+						}
+					}
+					return postsArray;
+				})
+			)
+			.subscribe((posts) => {
+				console.log(posts);
+			});
 	}
 }
